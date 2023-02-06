dbd9a0f2ca80d2c0953375ac006343aa54f7eb24
Piotr <p.marosz@poczta.onet.pl>
2023-02-06 17:13:31 +0100

    Initial commit

---
+++ src/main/java/com/xionix93/texec/TExecApplication.java
@@ -0,0 +1,12 @@
+package com.xionix93.texec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TExecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TExecApplication.class, args);
+    }
+}
